a86f10c25f92bc170c5e33ce3a981bdba8ffdbdb
Chr1SeS <christopherxxqs@gmail.com>
2025-07-11 14:44:01 +1000

    Revert "Drop in the MQTT customised plugin, but didn't rebuild successfully due to C4727 error"
    This reverts commit ca7494c50f5720c5d7fb318a7a2834f0362b85a8.

--- Source/Heartbeat51/Heartbeat51.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Heartbeat51.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Heartbeat51, "Heartbeat51" );
